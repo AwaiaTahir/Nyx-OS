0af67f8e2c773efd4d21cc2ff9b2dd7cb85f8816
Awais4929 <Awaistahir241417@gmail.com>
2025-04-30 03:43:56 -0400

    Implemented String Class

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <kernel/tty.h>
 #include <stdio.h>
-#include <string.h>
+#include <string>
 
 extern "C" void kernel_main(void) {
     terminal_initialize();
-    printf("Hello, kernel World!\n");
+    string s = "Awais";
+    string q = "Tahir";
+    string c = s * 3;
+    // printf(s);
+    // printf(q);
+    printf(c);
 }
--- libc++/string.cpp
+++ libc++/string.cpp
@@ -1,3 +1,38 @@
 #include <string>
 
-string::string() { }
+string::string(const char *s) { strcpy(Str, s); }
+string::string(string &s) { strcpy(Str, s.Str); }
+string  string::operator=(string s) { return strcpy(Str, s); }
+string::operator const char *() { return Str; }
+
+string string::operator+=(string s) { return strcat(Str, s); }
+
+string string::operator+(string s) {
+    string result = "";
+    strcpy(result.Str, Str);
+    strcat(result.Str, s);
+    return result;
+}
+
+string string::operator*(int s) {
+    string result = "";
+    for (int i = 0; i < s; i++) { strcat(result.Str, Str); }
+    return result;
+}
+
+string string::operator*=(int s) {
+    for (int i = 0; i < s; i++) { strcat(Str, Str); }
+    return Str;
+}
+
+bool string::operator==(string s) { return strcmp(Str, s) == 0; }
+
+bool string::operator<(string s) { return strcmp(Str, s) < 0; }
+
+bool string::operator<=(string s) { return strcmp(Str, s) <= 0; }
+
+bool string::operator>(string s) { return strcmp(Str, s) > 0; }
+
+bool string::operator>=(string s) { return strcmp(Str, s) >= 0; }
+
+char &string::operator[](int idx) { return Str[idx]; }
